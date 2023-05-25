39144ea252bfd73d0538216188e6e9ceb9e34ee8
GioF_71 <giovanni.fulco@gmail.com>
2023-05-25 10:00:47 +0200

    fix read config key with index 0

--- mpd_subsonic_scrobbler/indexed_key.py
+++ mpd_subsonic_scrobbler/indexed_key.py
@@ -2,11 +2,12 @@ from context_key import ContextKey
 from config_key import ConfigKey
 
 def indexed_context_key(key : ContextKey, index : int) -> str:
-    return __indexed_config_key(key = key.get_key(), index = index)
+    return __concat_key(key = key.get_key(), index = index)
 
 def indexed_config_key(key : ConfigKey, index : int) -> str:
-    return __indexed_config_key(key = key.get_key(), index = index)
+    if index == 0: return key.get_key()
+    return __concat_key(key = key.get_key(), index = index)
 
-def __indexed_config_key(key : str, index : int) -> str:
+def __concat_key(key : str, index : int) -> str:
     return f"{key}_{str(index)}"
 
