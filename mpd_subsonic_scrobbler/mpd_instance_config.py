7f478c07a38cf2104ddfb0dacfea87e6772699dc
GioF_71 <giovanni.fulco@gmail.com>
2023-05-25 09:53:23 +0200

    support default value for mpd, created indexed_key

--- mpd_subsonic_scrobbler/mpd_instance_config.py
+++ mpd_subsonic_scrobbler/mpd_instance_config.py
@@ -1,20 +1,19 @@
 from subsonic_connector.configuration import Configuration
 from config_util import get_indexed_env_value
+from config_util import get_env_value
 
 from config_key import ConfigKey
-
-import dotenv
-import libsonic
+from indexed_key import indexed_config_key
 
 class MpdInstanceConfig(Configuration):
 
     def __init__(self, index : int = 0):
-        self.__mpd_host : str = get_indexed_env_value(
-            ConfigKey.MPD_HOST.get_key(), 
-            index)
-        self.__mpd_port : int = int(get_indexed_env_value(
-            ConfigKey.MPD_PORT.get_key(), 
-            index))
+        self.__mpd_host : str =  self.__read_env(ConfigKey.MPD_HOST, index)
+        self.__mpd_port : int = int(self.__read_env(ConfigKey.MPD_PORT, index))
+
+    def __read_env(self, config_key : ConfigKey, index : int) -> str:
+        key : str = indexed_config_key(config_key, index)
+        return get_env_value(key, config_key.get_default_value())
 
     def get_mpd_host(self) -> str: return self.__mpd_host
     def get_mpd_port(self) -> int: return self.__mpd_port
